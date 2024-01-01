c4b7e1b0550fae2d41267b7194774bc99d00f4ff
Stéphane Cachat <scachat@sstinformatique.fr>
2024-01-01 18:44:50 +0100

    ajout d'une ui

--- reverse.py
+++ reverse.py
@@ -2,11 +2,10 @@ import tkinter as tk
 import math as math
 
 def calculK():
-    print("Simulation de vol d'un volant de 5g : calcul de k")
-    alpha = float(input("Angle de tir en degrés : "))*2*math.pi/360
-    v0 = float(input("Vitesse en m/s : "))
-    xa = float(input("Distance parcourue en m: "))
-    y0 = float(input("hauteur du point de départ en m: "))
+    alpha = float(alphaE.get())*2*math.pi/360
+    v0 = float(v0E.get())
+    xa = float(xaE.get())
+    y0 = float(y0E.get())
     k=0.001
     dk=0.1
     first=0
@@ -37,33 +36,38 @@ def calculK():
             print('+ k=',k,', x=',x,', t=',t,', xa=',xa)
             k=k+dk
     print('Le coefficient de frottement fluide est k=',k)
+    kE["text"]=f"{round(k, 8)}"
+
 
 
 window = tk.Tk()
 window.title('Analyse : calcul du coefficient de frottement dynamique')
 window.resizable(width=False, height=False)
-frm=tk.Frame(master=window)
-angleL= tk.Label(master=frm, text="Angle de tir en degrés")
-angleL.grid(row=1,column=0)
-angleE = tk.Entry(master=frm, width=10)
-angleE .grid(row=1,column=1)
-v0L= tk.Label(master=frm, text="Vitesse en m/s")
+alphaL= tk.Label(master=window, text="Angle de tir en degrés")
+alphaL.grid(row=1,column=0)
+alphaE = tk.Entry(master=window, width=10)
+alphaE .insert(0,"45")
+alphaE .grid(row=1,column=1)
+v0L= tk.Label(master=window, text="Vitesse en m/s")
 v0L.grid(row=2,column=0)
-v0E = tk.Entry(master=frm, width=10)
+v0E = tk.Entry(master=window, width=10)
+v0E .insert(0,"100")
 v0E .grid(row=2,column=1)
-xaL= tk.Label(master=frm, text="Distance parcourue en m")
+xaL= tk.Label(master=window, text="Distance parcourue en m")
 xaL.grid(row=3,column=0)
-xaE = tk.Entry(master=frm, width=10)
+xaE = tk.Entry(master=window, width=10)
+xaE .insert(0,"10")
 xaE .grid(row=3,column=1)
-y0L= tk.Label(master=frm, text="Hauteur de départ en m")
+y0L= tk.Label(master=window, text="Hauteur de départ en m")
 y0L.grid(row=4,column=0)
-y0E = tk.Entry(master=frm, width=10)
+y0E = tk.Entry(master=window, width=10)
+y0E .insert(0,"0.3")
 y0E .grid(row=4,column=1)
-ok = tk.Button(master=frm, text="Calculer",command=calculK)
+ok = tk.Button(master=window, text="Calculer",command=calculK)
 ok .grid(row=5,column=0)
-kL= tk.Label(master=frm, text="Coefficient de frottement fluide k")
+kL= tk.Label(master=window, text="Coefficient de frottement fluide k")
 kL.grid(row=6,column=0)
-kE= tk.Label(master=frm, text="...")
+kE= tk.Label(master=window, text="...")
 kE.grid(row=6,column=1)
 
 window.mainloop()
